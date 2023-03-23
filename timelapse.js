bb2ccbe15cda626a9ebda4ac070bd1e1fc3518c3
Jake Ellis <jmelli12@gmail.com>
2023-03-23 16:46:02 -0400

    Update timelapse.js

--- timelapse.js
+++ timelapse.js
@@ -22,3 +22,37 @@ async function streamFileDownload() {
 }
 
 streamFileDownload().catch(console.error);
+
+
+const date = new Date();
+console.log(date); // Fri Jun 17 2022 11:27:28 GMT+0100 (British Summer Time)
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+document.getElementById("day").innerHTML=day;
+document.getElementById("month").innerHTML=month;
+document.getElementById("year").innerHTML=year;
+
+  // Imports the Google Cloud client library
+const {Storage} = require('@google-cloud/storage');
+
+// Creates a client
+const storage = new Storage();
+
+async function readFiles () {
+  const [files] = await bucket.getFiles({ prefix: 'date-night-qa.appspot.com/parking_lot_timelapse/videos'});
+  console.log('Files:');
+  files.forEach(file => {
+    document.getElementById("videoName1").innerHTML=videoSrcUrl;
+  });
+};
+
+admin.storage().bucket()
+.getFiles({ prefix: 'date-night-qa.appspot.com/parking_lot_timelapse/videos/', autoPaginate: false })
+.then((files) => {
+document.getElementById("videoName2").innerHTML=videoSrcUrl;
+});
+
+let videoSrcUrl = "URL HERE"
+document.getElementById("videoUrl").src=videoSrcUrl;
