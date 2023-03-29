e8a0a90f2845da7b1f5a25bf65453fdc57482710
Jake Ellis <jmelli12@gmail.com>
2023-03-29 13:32:09 -0400

    Update timelapse.js

--- scripts/timelapse.js
+++ scripts/timelapse.js
@@ -4,9 +4,9 @@ let day = date.getDate();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
 
-document.getElementById("day").innerHTML=day;
-document.getElementById("month").innerHTML=month;
-document.getElementById("year").innerHTML=year;
+// document.getElementById("day").innerHTML=day;
+// document.getElementById("month").innerHTML=month;
+// document.getElementById("year").innerHTML=year;
 
 
 
@@ -15,33 +15,31 @@ document.getElementById("year").innerHTML=year;
 
 //const {Storage} = require('@google-cloud/storage');
 
-import * as storageModule from "@google-cloud/storage";
-const { Storage } = storageModule;
+// import * as storageModule from "@google-cloud/storage";
+// const { Storage } = storageModule;
 
-// Creates a client
-const storage = new Storage();
+// // Creates a client
+// const storage = new Storage();
 
-async function streamFileDownload() {
-  // The example below demonstrates how we can reference a remote file, then
-  // pipe its contents to a local file.
-  // Once the stream is created, the data can be piped anywhere (process, sdout, etc)
-  await storage
-    .bucket("date-night-qa.appspot.com/parking_lot_timelapse/videos/")
-    .file("20230326_155701.MP4")
-    .createReadStream() //stream is created
-    .pipe(fs.createWriteStream(destFileName))
-    .on('finish', () => {
-      document.getElementById("videoName1").innerHTML=videoSrcUrl;
-    });
+// async function streamFileDownload() {
+//   // The example below demonstrates how we can reference a remote file, then
+//   // pipe its contents to a local file.
+//   // Once the stream is created, the data can be piped anywhere (process, sdout, etc)
+//   await storage
+//     .bucket("date-night-qa.appspot.com/parking_lot_timelapse/videos/")
+//     .file("20230326_155701.MP4")
+//     .createReadStream() //stream is created
+//     .pipe(fs.createWriteStream(destFileName))
+//     .on('finish', () => {
+//       document.getElementById("videoName1").innerHTML=videoSrcUrl;
+//     });
 
-  console.log(
-    `gs://${bucketName}/${fileName} downloaded to ${destFileName}.`
-  );
-}
+//   console.log(
+//     `gs://${bucketName}/${fileName} downloaded to ${destFileName}.`
+//   );
+// }
 
-streamFileDownload().catch(console.error);
-
-
+// streamFileDownload().catch(console.error);
 
 
 
@@ -49,23 +47,25 @@ streamFileDownload().catch(console.error);
 
 
 
-async function readFiles () {
-  const [files] = await bucket.getFiles({ prefix: 'date-night-qa.appspot.com/parking_lot_timelapse/videos/'});
-  console.log('Files:');
-  files.forEach(file => {
-    console.log(file);
-    document.getElementById("videoName2").innerHTML="file2 ran";
-  });
-};
 
-/*
-admin.storage().bucket()
-.getFiles({ prefix: 'date-night-qa.appspot.com/parking_lot_timelapse/videos/', autoPaginate: false })
-.then((files) => {
-document.getElementById("videoName2").innerHTML=videoSrcUrl;
-});
 
-let videoSrcUrl = "URL HERE"
-document.getElementById("videoUrl").src=videoSrcUrl;
-*/
+// async function readFiles () {
+//   const [files] = await bucket.getFiles({ prefix: 'date-night-qa.appspot.com/parking_lot_timelapse/videos/'});
+//   console.log('Files:');
+//   files.forEach(file => {
+//     console.log(file);
+//     document.getElementById("videoName2").innerHTML="file2 ran";
+//   });
+// };
+
+// /*
+// admin.storage().bucket()
+// .getFiles({ prefix: 'date-night-qa.appspot.com/parking_lot_timelapse/videos/', autoPaginate: false })
+// .then((files) => {
+// document.getElementById("videoName2").innerHTML=videoSrcUrl;
+// });
+
+// let videoSrcUrl = "URL HERE"
+// document.getElementById("videoUrl").src=videoSrcUrl;
+// */
 
